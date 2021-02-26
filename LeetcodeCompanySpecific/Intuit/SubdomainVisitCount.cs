b6cff7fd287e8e292da9003787e24d3f161afe3d
Novonil Das <das.novonil@gmail.com>
2021-02-25 23:07:59 -0600

    SubdomainVisitCount Solved.
    https://leetcode.com/problems/subdomain-visit-count/

---
+++ LeetcodeCompanySpecific/Intuit/SubdomainVisitCount.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCompanySpecific.Intuit
+{
+	class SubdomainVisitCount
+	{
+
+		/// <summary>
+		/// SubdomainVisits
+		/// Time Complexity - O(3n) + O(3n) = O(6n) = O(n)
+		/// Space Complexity - O(n)
+		/// </summary>
+		/// <param name="cpdomains"></param>
+		/// <returns></returns>
+		public IList<string> SubdomainVisits(string[] cpdomains)
+		{
+			Dictionary<string, long> domainVisitCounter = new Dictionary<string, long>();
+			IList<string> result = new List<string>();
+
+			foreach(string s in cpdomains)
+			{
+				int i = 0;
+				string[] splitData = s.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+				long counter = Convert.ToInt32(splitData[0]);
+				string domain = splitData[1];
+
+				while(i != -1)
+				{
+					if (domainVisitCounter.ContainsKey(domain))
+					{
+						domainVisitCounter[domain] += counter;
+					}
+					else
+					{
+						domainVisitCounter.Add(domain, counter);
+					}
+					i = domain.IndexOf(".");
+					domain = domain.Substring(i + 1);
+				}
+			}
+			foreach (KeyValuePair<string, long> kv in domainVisitCounter)
+			{
+				result.Add(kv.Value + " " + kv.Key);
+			}
+			return result;
+		}
+	}
+}
--- LeetcodeCompanySpecific/Program.cs
+++ LeetcodeCompanySpecific/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LeetcodeCompanySpecific.Intuit;
+using System;
+using System.Collections.Generic;
 
 namespace LeetcodeCompanySpecific
 {
@@ -6,7 +8,14 @@ namespace LeetcodeCompanySpecific
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string[] cpdomains = { "900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org" };
+			IList<string> res = new List<string>();
+			SubdomainVisitCount svc = new SubdomainVisitCount();
+			res = svc.SubdomainVisits(cpdomains);
+
+			foreach (string s in res)
+				Console.WriteLine(s);
+			Console.ReadLine();
 		}
 	}
 }
